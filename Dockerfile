9ad332b7df1bd8a0e3f733f15a535fe2ce9fe4f2
Arno Cellarier <git@arno.cl>
2025-04-29 17:55:20 +0200

    generate and upload osm.bz2 files

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,11 @@ RUN apk update && \
 
 RUN wget https://github.com/opentripplanner/OpenTripPlanner/releases/download/v2.7.0/otp-shaded-2.7.0.jar -O otp.jar
 
+RUN wget https://github.com/openstreetmap/osmosis/releases/download/0.49.2/osmosis-0.49.2.zip && \
+    unzip osmosis-0.49.2.zip -d /opt && \
+    mv /opt/osmosis-0.49.2 /opt/osmosis && \
+    ln -s /opt/osmosis/bin/osmosis /usr/local/bin/osmosis
+
 RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 
 RUN apk add --no-cache s5cmd
--- tasks.sh
+++ tasks.sh
@@ -1,5 +1,22 @@
 set -e
 
+wget https://download.geofabrik.de/europe/cyprus-latest.osm.pbf
+wget https://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf
+
+osmosis \
+  --read-pbf cyprus-latest.osm.pbf \
+  --write-xml cyprus-latest.osm.bz2
+
+osmosis \
+  --read-pbf ile-de-france-latest.osm.pbf \
+  --write-xml file=ile-de-france-latest.osm.bz2
+
+s5cmd --endpoint-url="${AWS_ENDPOINT_URL}" \
+  cp ./cyprus-latest.osm.bz2 "s3://${AWS_S3_BUCKET}/cyprus-latest.osm.bz2"
+
+s5cmd --endpoint-url="${AWS_ENDPOINT_URL}" \
+  cp ./ile-de-france-latest.osm.bz2 "s3://${AWS_S3_BUCKET}/ile-de-france-latest.osm.bz2"
+
 java -Xmx20G -jar otp.jar --buildStreet .
 
 s5cmd --endpoint-url="${AWS_ENDPOINT_URL}" \
